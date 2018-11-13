70bbb55b79fe619979a6261a8e56af1704556959
Akram Abdelrahim <zapemz@gmail.com>
2018-11-13 14:26:06 -0600

    add insecure flag to git

---
+++ say/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	pb "github.com/aabdelrahim/grpc-say/api"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// lookup parse and flag
+	backend := flag.String("b", "localhost:8080", "address of the say backend")
+	output := flag.String("o", "output.wav", "wav sound output")
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		fmt.Printf("usage:\n\t%s \"text to speak\"\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	conn, err := grpc.Dial(*backend, grpc.WithInsecure())
+	if err != err {
+		log.Fatalf("could not dial backend %s: %v", *backend, err)
+	}
+	defer conn.Close()
+
+	client := pb.NewTextToSpeechClient(conn)
+	request := &pb.SayRequest{Text: os.Args[1]}
+	response, err := client.Say(context.Background(), request)
+	if err != nil {
+		log.Fatalf("could not say %s: %v", request.Text, err)
+	}
+	err = ioutil.WriteFile(*output, response.Audio, 777)
+	if err != nil {
+		log.Fatalf("could not write to file %s: %v", *output, err)
+	}
+
+}
